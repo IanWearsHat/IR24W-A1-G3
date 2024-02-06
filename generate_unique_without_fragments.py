3017b86b34189ef92dd6f66c418e4b513f039a5e
Xiao Du <xdu7@circinus-48.ics.uci.edu>
2024-02-06 15:31:41 -0800

    Xiao Du unique page part

---
+++ generate_unique_without_fragments.py
@@ -0,0 +1,23 @@
+def url_without_fragment(url):
+    sign = url.find("#")
+    if sign == -1:
+        return url
+    return url[: sign]
+
+
+def read_delete_fragment(path="urls.txt"):
+    result = set()
+    with open(path, "r") as fr:
+        for url in fr.readlines():
+            url_no_fragment = url_without_fragment(url)
+            result.add(url_no_fragment)
+    with open("url_no_fragments.txt", "w") as fw:
+        out = []
+        for url in result:
+            out.append(url)
+        fw.writelines(out)
+    print(f"Number of unique urls is {len(out)}")
+
+
+if __name__ == '__main__':
+    read_delete_fragment()
